b42c664b03bc35cef785069da71c20d776439823
Aisha Blake <aisha.g.blake@gmail.com>
2020-02-13 03:20:43 -0500

    Step 2: GET /cart-items/:id

--- cart-items-routes.js
+++ cart-items-routes.js
@@ -9,4 +9,13 @@ cartItems.get("/", (req, res) => {
   res.json(cart);
 });
 
+cartItems.get("/:id", (req, res) => {
+  let selectedItem = cart.find(item => item.id === req.params.id);
+  if (selectedItem) {
+    res.json(selectedItem);
+  } else {
+    res.status(404).json("That item isn't in this cart!");
+  }
+});
+
 module.exports = cartItems;
